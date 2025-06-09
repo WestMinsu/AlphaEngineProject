6a38c8bc5617bb31ecfd0dc5076d13d42b4f5d7e
WestMinsu <d2v210p2r@naver.com>
2025-06-09 22:21:07 +0900

    add project file

---
+++ game/game/IntroState.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "AGameState.h"
+class IntroState: public AGameState
+{
+};
---
+++ game/game/MainGameState.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "AGameState.h"
+class MainGameState : public AGameState
+{
+};
