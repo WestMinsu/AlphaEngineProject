632fccd5d4e29da39ca5d24588f7b6febf58125e
Fenr1r <4p0the0sis94@gmail.com>
2025-06-10 11:13:27 +0900

    Update GameManager.h
    - Declare memeber functions and variables

--- game/game/GameManager.h
+++ game/game/GameManager.h
@@ -1,4 +1,35 @@
 #pragma once
+#include "AEEngine.h"
+#include "AGameState.h"
+
 class GameManager
 {
+public:
+	// constructor
+	GameManager();
+
+	// destructor
+	~GameManager();
+
+	void Init();
+	void Update();
+
+	//static member variable
+	// width & height of the window
+	static s32 m_widthWindow;
+	static s32 m_heightWindow;
+
+	// GameState variables to change
+	static AGameState* m_currentState; 
+	static AGameState* m_nextState;
+
+	// Game Font
+	static s8 m_font;
+
+	// static function
+	static void ChangeState();
+
+private:
+	bool isGameRunning;
+
 };
