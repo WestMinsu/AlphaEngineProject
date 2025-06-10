ebd40ff6c07154d68e4689aa3acffe13f5b43e44
Fenr1r <4p0the0sis94@gmail.com>
2025-06-10 15:34:58 +0900

    GameStates' function
    - Make functions' Definition
    - Need to implement

--- game/game/IntroState.cpp
+++ game/game/IntroState.cpp
@@ -1,1 +1,27 @@
 #include "IntroState.h"
+
+
+IntroState::IntroState()
+{
+}
+
+IntroState::~IntroState()
+{
+}
+
+void IntroState::Init()
+{
+}
+
+void IntroState::Update()
+{
+
+}
+
+void IntroState::Draw()
+{
+}
+
+void IntroState::Exit()
+{
+}
