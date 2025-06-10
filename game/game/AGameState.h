791d12ef8b32a147b5f3354b3ec420ae12fe4a9b
Fenr1r <4p0the0sis94@gmail.com>
2025-06-10 11:24:10 +0900

    Update AGameState.h
    - Declare virtaul Functions

--- game/game/AGameState.h
+++ game/game/AGameState.h
@@ -1,4 +1,13 @@
 #pragma once
+
+
 class AGameState
 {
+public: 
+	virtual ~AGameState() {};
+
+	virtual void Init() = 0;
+	virtual void Update() = 0;
+	virtual void Print() = 0;
+	virtual void Exit() = 0;
 };
