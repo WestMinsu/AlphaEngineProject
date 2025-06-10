6b0d2a04fb1dde21b299d63abe73088b8e6702ad
Fenr1r <4p0the0sis94@gmail.com>
2025-06-10 15:32:57 +0900

    Update IntroState.h
    - Declare functions and variables

--- game/game/IntroState.h
+++ game/game/IntroState.h
@@ -2,4 +2,17 @@
 #include "AGameState.h"
 class IntroState: public AGameState
 {
+public:
+	IntroState();
+
+	~IntroState();
+
+	// Override Functions
+	void Init() override;
+	void Update() override;
+	void Draw() override;
+	void Exit() override;
+
+private:
+
 };
