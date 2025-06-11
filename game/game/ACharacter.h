e7e99f4a450694f8f878e2f3812f1d6e982ec8d8
Fenr1r <4p0the0sis94@gmail.com>
2025-06-11 12:04:47 +0900

    Update ACharacter.h
    Declare virtual functions and private variables

--- game/game/ACharacter.h
+++ game/game/ACharacter.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "AEEngine.h"
+
 class ACharacter
 {
+public:
+	~ACharacter() {};
+	virtual void Init() = 0;
+	virtual void Update() = 0;
+	virtual void Move() = 0;
+	virtual void Attack() = 0;
+
+private:
+	AEVec2 m_position;
+	int m_healthPoint;
 };
 
