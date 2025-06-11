7e14b1aa63aa2dc7f492a8bb8fd19a531b05a57c
Fenr1r <4p0the0sis94@gmail.com>
2025-06-11 16:44:33 +0900

    Create Animation.h
    - Declare functions and variables

---
+++ game/game/Animation.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <AEEngine.h>
+#include "ACharacter.h"
+
+class Animation
+{
+public:
+	Animation();
+	~Animation();
+
+	void Init();
+	void Update(f32 dt);
+	void Draw();
+	void Destroy();
+
+private:
+	AEGfxVertexList* m_mesh;
+
+	AEGfxTexture* m_pTexIdle;
+	AEGfxTexture* m_pTexWalk;
+	AEGfxTexture* m_pTexJump;
+	AEGfxTexture* m_pTexDeath;
+	AEGfxTexture* m_currentPTex;
+
+	s32 m_subImageIndex;
+	f32 m_offset;
+	f32 m_elapsedTime;
+
+	s32 m_idleFrames = 8;
+	s32 m_walkFrames = 8;
+	s32 m_jumpFrames = 8;
+	s32 m_deathFrames = 8;
+
+	bool m_animationFinished = false;
+	f32 m_deathTimer;
+	const f32 m_restartDelay = 3.0f;
+};
