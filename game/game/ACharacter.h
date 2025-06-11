248f58054e42633562f33bdf0357e78b640edecd
Fenr1r <4p0the0sis94@gmail.com>
2025-06-11 15:28:34 +0900

    Update ACharacter.h
    - Change private to protected

--- game/game/ACharacter.h
+++ game/game/ACharacter.h
@@ -1,18 +1,24 @@
 #pragma once
 #include "AEEngine.h"
+#include "MainGameState.h"
 
 class ACharacter
 {
 public:
 	~ACharacter() {};
-	virtual void Init() = 0;
+	virtual void Init(AEVec2 position) = 0;
 	virtual void Update() = 0;
 	virtual void Move() = 0;
 	virtual void Attack() = 0;
 	virtual void Draw() = 0;
 
-private:
+protected:
 	AEVec2 m_position;
+	AEVec2 m_size;
+
 	int m_healthPoint;
+	f32 m_characterSpeed = 300.0f;
+
+	CharacterDirection m_currentDirection;
 };
 
