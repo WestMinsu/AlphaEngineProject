e9f49d9251fe5013b751a10b060c34ecee516169
Fenr1r <4p0the0sis94@gmail.com>
2025-06-11 15:29:30 +0900

    Change CharacterPlayer
    - Add private variables

--- game/game/CharacterPlayer.cpp
+++ game/game/CharacterPlayer.cpp
@@ -2,22 +2,35 @@
 
 CharacterPlayer::CharacterPlayer()
 {
+	// TO DO change 
+	// Maybe in Constants
+	m_position = { 0,0 };
+	m_size = { 500.f, 500.f };
+	m_healthPoint = 100;
+	m_characterSpeed = 300.f;
+	m_currentAnimState = CharacterAnimationState::IDLE;
+	m_currentDirection = CharacterDirection::RIGHT;
+
 }
+
 
 CharacterPlayer::~CharacterPlayer()
 {
 }
 
-void CharacterPlayer::Init()
+void CharacterPlayer::Init(AEVec2 position)
 {
+	m_position = position;
 }
 
 void CharacterPlayer::Update()
 {
+
 }
 
 void CharacterPlayer::Move()
 {
+
 }
 
 void CharacterPlayer::Attack()
@@ -26,4 +39,16 @@ void CharacterPlayer::Attack()
 
 void CharacterPlayer::Draw()
 {
+
+
+}
+
+CharacterAnimationState CharacterPlayer::getCurrentAnimState()
+{
+	return m_currentAnimState;
+}
+
+void CharacterPlayer::setCurrentAnimState(CharacterAnimationState newAnimState)
+{
+	m_currentAnimState = newAnimState;
 }
--- game/game/CharacterPlayer.h
+++ game/game/CharacterPlayer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ACharacter.h"
+#include "MainGameState.h"
 
 class CharacterPlayer : public ACharacter
 {
@@ -7,13 +8,21 @@ public:
 	CharacterPlayer();
 	~CharacterPlayer();
 
-	void Init() override;
+	void Init(AEVec2 m_position) override;
 	void Update() override;
 	void Move() override;
 	void Attack() override;
 	void Draw() override;
 
+	//getter 
+	CharacterAnimationState getCurrentAnimState();
+
+	//setter
+	void setCurrentAnimState(CharacterAnimationState newAnimState);
+
 private:
 	// Weapon
+	CharacterAnimationState m_currentAnimState;
+	
 };
 
