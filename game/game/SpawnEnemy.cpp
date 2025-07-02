e406a87131103e31d22247acc299a7bc93ffa94f
Fenr1r <4p0the0sis94@gmail.com>
2025-07-02 15:21:09 +0900

    Update SpawnEnemy
    - Fix functions' Parameters
    - Add string m_enemyName
    - Implement a function, Update

--- game/game/SpawnEnemy.cpp
+++ game/game/SpawnEnemy.cpp
@@ -1,12 +1,24 @@
 #include "SpawnEnemy.h"
 
 
-SpawnEnemy::SpawnEnemy(AEVec2 pos)
+SpawnEnemy::SpawnEnemy()
 {
-	m_position = pos;
-	m_spawnTimes = 5;
+	m_position = {0,0};
+	m_spawnTimes = 1;
 	m_spawnCurrentTime = 0.0f;
 	m_spawnTerm = 2.0f;
+	m_EnemyFactory = nullptr;
+	m_enemyName = "";
+}
+
+SpawnEnemy::SpawnEnemy(AEVec2 pos, EnemyFactory* factory, std::string name)
+{
+	m_position = pos;
+	m_spawnTimes = 1;
+	m_spawnCurrentTime = 0.0f;
+	m_spawnTerm = 2.0f;
+	m_EnemyFactory = factory;
+	m_enemyName = name;
 }
 
 SpawnEnemy::~SpawnEnemy()
@@ -14,12 +26,28 @@ SpawnEnemy::~SpawnEnemy()
 
 }
 
+void SpawnEnemy::Init(AEVec2 pos, EnemyFactory* factory, std::string name)
+{
+	m_position = pos;
+	m_EnemyFactory = factory;
+	m_enemyName = name;
+}
+
 void SpawnEnemy::Update(f32 dt, std::vector<ACharacter*>& enemies)
 {
-	
+	m_spawnCurrentTime += dt;
+	if (m_spawnTimes > 0 && m_spawnCurrentTime > m_spawnTerm)
+	{
+		ACharacter* newEnemy = m_EnemyFactory->Create(m_enemyName);
+		newEnemy->Init(m_position);
+		enemies.push_back(newEnemy);
+		m_spawnTimes--;
+		m_spawnCurrentTime = 0.f;
+	}
 }
 
 void SpawnEnemy::Destroy()
 {
+
 }
 
--- game/game/SpawnEnemy.h
+++ game/game/SpawnEnemy.h
@@ -7,9 +7,11 @@
 class SpawnEnemy
 {
 public:
-	SpawnEnemy(AEVec2 pos);
+	SpawnEnemy();
+	SpawnEnemy(AEVec2 pos, EnemyFactory* factory, std::string name);
 	~SpawnEnemy();
 
+	void Init(AEVec2 pos, EnemyFactory* factory, std::string name);
 	void Update(f32 dt, std::vector<ACharacter*>&);
 	void Destroy();
 
@@ -19,6 +21,7 @@ private:
 
 	f32 m_spawnCurrentTime;
 	f32 m_spawnTerm;
+	std::string m_enemyName;
 	EnemyFactory* m_EnemyFactory;
 };
 
