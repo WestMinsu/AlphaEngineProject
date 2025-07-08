3791b17cea334bb058c2d6e50e4c7a6ff4b1ef8d
Fenr1r <4p0the0sis94@gmail.com>
2025-07-08 20:26:46 +0900

    Update SpawnEnemy
    - Add Update for overloading

--- game/game/SpawnEnemy.cpp
+++ game/game/SpawnEnemy.cpp
@@ -5,7 +5,7 @@
 SpawnEnemy::SpawnEnemy()
 {
 	m_position = {0,0};
-	m_spawnTimes = 10;
+	m_spawnTimes = 1;
 	m_spawnCurrentTime = 0.0f;
 	m_spawnTerm = 2.0f;
 	m_EnemyFactory = nullptr;
@@ -13,10 +13,10 @@ SpawnEnemy::SpawnEnemy()
 	m_resetCount = 0;
 }
 
-SpawnEnemy::SpawnEnemy(AEVec2 pos, EnemyFactory* factory, std::string name)
+SpawnEnemy::SpawnEnemy(AEVec2 pos, EnemyFactory* factory, std::string name, s32 spawnTimes)
 {
 	m_position = pos;
-	m_spawnTimes = 10;
+	m_spawnTimes = spawnTimes;
 	m_spawnCurrentTime = 0.0f;
 	m_spawnTerm = 2.0f;
 	m_EnemyFactory = factory;
@@ -29,17 +29,20 @@ SpawnEnemy::~SpawnEnemy()
 
 }
 
-void SpawnEnemy::Init(AEVec2 pos, EnemyFactory* factory, std::string name)
+void SpawnEnemy::Init(AEVec2 pos, EnemyFactory* factory, std::string name, s32 spawnTimes)
 {
 	m_position = pos;
+	m_spawnTimes = spawnTimes;
 	m_EnemyFactory = factory;
 	m_enemyName = name;
 }
 
-void SpawnEnemy::Update(f32 dt, std::vector<ACharacter*>& enemies)
+
+void SpawnEnemy::Update(f32 dt, AEVec2 positionPlayer, std::vector<ACharacter*>& enemies)
 {
 	m_spawnCurrentTime += dt;
-	if (m_spawnTimes > 0 && m_spawnCurrentTime >= m_spawnTerm)
+	if (m_spawnTimes > 0 
+		&& m_spawnCurrentTime >= m_spawnTerm)
 	{
 		ACharacter* newEnemy = m_EnemyFactory->Create(m_enemyName);
 		newEnemy->Init(m_position);
@@ -52,6 +55,23 @@ void SpawnEnemy::Update(f32 dt, std::vector<ACharacter*>& enemies)
 		Reset();
 	}
 }
+
+
+void SpawnEnemy::Update(f32 dt, AEVec2 positionPlayer, std::vector<BossCharacter*>& bosses)
+{
+	m_spawnCurrentTime += dt;
+	if (m_spawnTimes > 0 
+		&& m_position.x - positionPlayer.x < kHalfWindowWidth)
+	{
+		BossCharacter* newBoss = dynamic_cast<BossCharacter*>(m_EnemyFactory->Create(m_enemyName));
+		newBoss->Init(m_position);
+		bosses.push_back(newBoss);
+		m_spawnTimes--;
+		m_spawnCurrentTime = 0.f;
+	}
+}
+
+
 
 void SpawnEnemy::Destroy()
 {
