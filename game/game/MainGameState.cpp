ff58a6f627aa42fa01b7aa99a14cf938b8fe30ed
Fenr1r <4p0the0sis94@gmail.com>
2025-07-08 20:26:05 +0900

    Update BossCharater
    - Add Copy Constructor
    - Refactoring MainGameState
    - Add functions from MainGameState

--- game/game/MainGameState.cpp
+++ game/game/MainGameState.cpp
@@ -24,7 +24,7 @@ void MainGameState::Init()
 
 	TileMaps.push_back(TileMap("Assets/Maps/test0_32.tmj", 2.f));
 	TileMaps.push_back(TileMap("Assets/Maps/test1_32.tmj", 2.f, TileMaps[0].GetMapTotalWidth()));
-	TileMaps.push_back(TileMap("Assets/Maps/test2_32.tmj", 2.f, TileMaps[0].GetMapTotalWidth()*6));
+	TileMaps.push_back(TileMap("Assets/Maps/test2_32.tmj", 2.f, TileMaps[0].GetMapTotalWidth() * 6));
 
 	m_Background.Init();
 	WarriorEnemyCharacter* warrior = new WarriorEnemyCharacter();
@@ -35,24 +35,24 @@ void MainGameState::Init()
 	fire->Init({ kHalfWindowWidth - 550.f, 0.f }, &m_Player);
 	NightBorneEnemyCharacter* night = new NightBorneEnemyCharacter();
 	night->Init({ kHalfWindowWidth - 550.f, 100.f }, &m_Player);
+	BossCharacter* boss = new BossCharacter();
+	boss->Init({ TileMaps[0].GetMapTotalWidth() * 7.f - kWindowWidth - boss->GetSize().x / 2.f, -150.f }, &m_Player);
 
-	//m_factory = std::make_shared<EnemyFactory>();
 	m_factory.RegisterPrototype("Warrior", warrior);
 	m_factory.RegisterPrototype("Mage", mage);
 	m_factory.RegisterPrototype("Fire", fire);
 	m_factory.RegisterPrototype("Night", night);
+	m_factory.RegisterPrototype("Boss", boss);
 
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 1350, -kHalfWindowHeight + 270 }, &m_factory, "Warrior"));
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 1660, -kHalfWindowHeight + 680 }, &m_factory, "Mage"));
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2150, -kHalfWindowHeight + 330 }, &m_factory, "Warrior"));
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2820, -kHalfWindowHeight + 750 }, &m_factory, "Mage"));
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2950, -kHalfWindowHeight + 425 }, &m_factory, "Fire"));
-	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 3425, -kHalfWindowHeight + 585 }, &m_factory, "Night"));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 1350, -kHalfWindowHeight + 270 }, &m_factory, "Warrior", 6));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 1660, -kHalfWindowHeight + 680 }, &m_factory, "Mage", 4));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2250, -kHalfWindowHeight + 330 }, &m_factory, "Warrior", 8));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2820, -kHalfWindowHeight + 750 }, &m_factory, "Mage", 5));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 2950, -kHalfWindowHeight + 425 }, &m_factory, "Fire", 4));
+	m_Spawns.push_back(new SpawnEnemy({ -kHalfWindowWidth + 3425, -kHalfWindowHeight + 585 }, &m_factory, "Night", 2));
 
-	//m_Boss.Init({ kHalfWindowWidth - 300.f, 100.f }, &m_Player);
-	m_Boss.Init({ TileMaps[0].GetMapTotalWidth() * 7.f - kWindowWidth -m_Boss.GetSize().x/2.f, -100.f}, &m_Player);
-
-	m_Enemies.push_back(&m_Boss);
+	//m_Boss.Init({ TileMaps[0].GetMapTotalWidth() * 7.f - kWindowWidth -m_Boss.GetSize().x/2.f, -100.f}, &m_Player);
+	m_SpawnBoss.Init( { TileMaps[0].GetMapTotalWidth() * 7.f - kWindowWidth, -100.f }, & m_factory, "Boss", 1);
 
 	m_pUiSlot = LoadImageAsset("Assets/UI/slot.png");
 	m_weaponIconMap[DamageType::FIRE] = LoadImageAsset("Assets/MagicArrow/fire_icon.png");
@@ -88,7 +88,7 @@ void MainGameState::Update(f32 dt)
 		}
 	}
 
-	if (m_Boss.IsCompletelyDead())
+	if (m_Bosses.size() > 0 && m_Bosses[0]->IsCompletelyDead())
 	{
 		GameManager::ChangeState(GameState::GAME_CLEAR);
 		return;
@@ -120,32 +120,25 @@ void MainGameState::Update(f32 dt)
 
 	m_Player.Update(dt);
 
-	m_Boss.Update(dt);
-
 	for (auto& spawn : m_Spawns)
 	{
-		spawn->Update(dt, m_Enemies);
+		spawn->Update(dt, m_Player.GetPosition(),  m_Enemies);
 	}
+
+	m_SpawnBoss.Update(dt, m_Player.GetPosition(), m_Bosses);
 
 	for (auto enemy : m_Enemies)
 	{
 		enemy->Update(dt);
 	}
 
-	BossAIState currentBossState = m_Boss.GetCurrentAIState();
-	if (currentBossState != m_previousBossAIState)
+	if (m_Bosses.size() > 0)
 	{
-		if (currentBossState == BossAIState::GLOWING)
-		{
-			m_pBossMessageTexture = LoadImageAsset("Assets/UI/healtext.png");
-			m_bossMessageTimer = m_bossMessageDuration;
-		}
-		else if (currentBossState == BossAIState::BUFF)
-		{
-			m_pBossMessageTexture = LoadImageAsset("Assets/UI/enragetext.png");
-			m_bossMessageTimer = m_bossMessageDuration;
-		}
-		m_previousBossAIState = currentBossState;
+		m_Bosses[0]->Update(dt);
+		m_Bosses[0]->GetBossPattern(m_pBossMessageTexture, m_bossMessageTimer, m_previousBossAIState);
+		m_Bosses[0]->AttackMelee(m_Player);
+		m_Bosses[0]->AttackRange(m_Player, m_enemyProjectiles);
+		m_Bosses[0]->AttackLaser(m_Player);
 	}
 
 	if (m_bossMessageTimer > 0.0f)
@@ -243,102 +236,6 @@ void MainGameState::Update(f32 dt)
 				newProjectile.Init(rangeEnemy->GetPosition(), directionVec, projData);
 
 				rangeEnemy->SetFiredProjectile(true);
-			}
-		}
-	}
-
-
-	bool isBossMeleeAttacking = m_Boss.GetCurrentAIState() == BossAIState::MELEE_ATTACK;
-	bool isBossHitboxActive = m_Boss.GetAnimation().GetCurrentFrame() >= 3;
-	if (isBossMeleeAttacking && isBossHitboxActive && !m_Boss.HasHitPlayerThisAttack() && m_Player.GetHealth() > 0)
-	{
-		const AttackHitbox& bossHitbox = m_Boss.GetCurrentMeleeHitbox();
-		AEVec2 bossPos = m_Boss.GetPosition();
-		CharacterDirection bossDir = m_Boss.GetDirection();
-		AEVec2 bossHitboxPos;
-		bossHitboxPos.x = bossPos.x + (bossDir == CharacterDirection::RIGHT ? bossHitbox.offset.x : -bossHitbox.offset.x);
-		bossHitboxPos.y = bossPos.y + bossHitbox.offset.y;
-
-		AEVec2 playerHitboxPos = m_Player.GetPosition();
-		playerHitboxPos.x += m_Player.GetHitboxOffset().x;
-		playerHitboxPos.y += m_Player.GetHitboxOffset().y;
-
-		if (CheckAABBCollision(bossHitboxPos, bossHitbox.size, playerHitboxPos, m_Player.GetHitboxSize()))
-		{
-			m_Player.TakeDamage(15, DamageType::NONE);
-			m_Boss.RegisterPlayerHit();
-		}
-	}
-
-	bool isBossCastingRanged = m_Boss.GetCurrentAnimState() == CharacterAnimationState::RANGED_ATTACK;
-	if (isBossCastingRanged && m_Boss.GetAnimation().IsFinished() && !m_Boss.HasFired())
-	{
-		m_Boss.SetFired(true);
-		const ProjectileData& projData = m_Boss.GetProjectileData();
-		AEVec2 bossPos = m_Boss.GetPosition();
-		AEVec2 playerPos = m_Player.GetPosition();
-
-		if (m_Boss.IsBuffed())
-		{
-			// 3-Way Shot
-			float baseAngle = atan2(playerPos.y - bossPos.y, playerPos.x - bossPos.x);
-			AEVec2 dirMid = { cosf(baseAngle), sinf(baseAngle) };
-			AEVec2 dirUp = { cosf(baseAngle - 0.3f), sinf(baseAngle - 0.3f) };
-			AEVec2 dirDown = { cosf(baseAngle + 0.3f), sinf(baseAngle + 0.3f) };
-
-			m_enemyProjectiles.emplace_back().Init(bossPos, dirUp, projData);
-			m_enemyProjectiles.emplace_back().Init(bossPos, dirMid, projData);
-			m_enemyProjectiles.emplace_back().Init(bossPos, dirDown, projData);
-		}
-		else
-		{
-			AEVec2 finalDir = { (m_Boss.GetDirection() == CharacterDirection::RIGHT ? 1.f : -1.f), 0.f };
-			float yDiff = playerPos.y - bossPos.y;
-			if (yDiff > 150.f) finalDir.y = 0.5f;
-			else if (yDiff < -150.f) finalDir.y = -0.5f;
-
-			AEVec2Normalize(&finalDir, &finalDir);
-			m_enemyProjectiles.emplace_back().Init(bossPos, finalDir, projData);
-		}
-	}
-
-	bool isBossLaserAttacking = m_Boss.GetCurrentAIState() == BossAIState::LASER_BEAM;
-	bool isLaserHitboxActive = m_Boss.GetLaserAnimation().GetCurrentFrame() >= 8;
-
-	if (isBossLaserAttacking && isLaserHitboxActive && !m_Boss.IsUnbeatable() && m_Player.GetHealth() > 0)
-	{
-		const AttackHitbox& laserData = m_Boss.GetLaserHitbox();
-		float offsetDir = (m_Boss.GetDirection() == CharacterDirection::RIGHT) ? 1.0f : -1.0f;
-		AEVec2 laserBasePos = { m_Boss.GetPosition().x + (laserData.offset.x * offsetDir), m_Boss.GetPosition().y + laserData.offset.y };
-
-		AEVec2 playerHitboxPos = m_Player.GetPosition();
-		playerHitboxPos.x += m_Player.GetHitboxOffset().x;
-		playerHitboxPos.y += m_Player.GetHitboxOffset().y;
-
-		if (m_Boss.IsBuffed())
-		{
-			// 3-Way Laser
-			float yOffsets[] = { 150.f, 0.f, -150.f };
-			for (float yOffset : yOffsets)
-			{
-				AEVec2 currentLaserPos = { laserBasePos.x, laserBasePos.y + yOffset };
-				if (CheckAABBCollision(currentLaserPos, laserData.size, playerHitboxPos, m_Player.GetHitboxSize()))
-				{
-					m_Player.TakeDamage(1, DamageType::NONE);
-					break;
-				}
-			}
-		}
-		else
-		{
-			AEVec2 finalLaserPos = laserBasePos;
-			float yDiff = m_Player.GetPosition().y - m_Boss.GetPosition().y;
-			if (yDiff > 150.f) finalLaserPos.y += 100.f;
-			else if (yDiff < -150.f) finalLaserPos.y -= 100.f;
-
-			if (CheckAABBCollision(finalLaserPos, laserData.size, playerHitboxPos, m_Player.GetHitboxSize()))
-			{
-				m_Player.TakeDamage(1, DamageType::NONE);
 			}
 		}
 	}
@@ -491,9 +388,9 @@ void MainGameState::Draw()
 		enemy->Draw();
 	}
 
-	if (!m_Boss.IsCompletelyDead())
+	if ( m_Bosses.size() > 0 && !m_Bosses[0]->IsCompletelyDead())
 	{
-		m_Boss.Draw();
+		m_Bosses[0]->Draw();
 	}
 
 	m_Player.Draw();
@@ -542,7 +439,18 @@ void MainGameState::Exit()
 	m_Background.Destroy();
 	m_Player.Destroy();
 
-	m_Boss.Destroy();
+	for (auto spawn : m_Spawns)
+	{
+		spawn->Destroy();
+	}
+
+	m_Spawns.clear();
+	m_SpawnBoss.Destroy();
+	m_factory.Destroy();
+
+	// To Do
+	m_Bosses[0]->Destroy();
+
 	for (auto& projectile : m_playerProjectiles) projectile.Destroy();
 	for (auto& projectile : m_enemyProjectiles) projectile.Destroy();
 	m_playerProjectiles.clear();
@@ -600,18 +508,22 @@ void MainGameState::DrawUI()
 	AEVec2 fontPos = GetNormalizedCoords(kHalfWindowWidth * 0.5f, kHalfWindowHeight - 40.f);
 	AEGfxPrint(GameManager::m_font, scoreBuffer, fontPos.x, fontPos.y, TextScale, 1, 1, 1, 1);
 
-	if (m_Boss.IsAttackable())
+	if (m_Bosses.size() > 0)
 	{
-		const float barWidth = 500.f;
-		const float barHeight = 125.f;
-		const float barX = 0;
-		const float barY = kHalfWindowHeight - 150.f;
-		DrawRect(barX + xCam, barY, barWidth, barHeight, 0.1f, 0.1f, 0.1f, 1.f);
-		float healthRatio = static_cast<float>(m_Boss.GetHealth()) / m_Boss.getMaxHealth();
-		float currentHealthWidth = barWidth * healthRatio;
-		DrawRect(barX + xCam - (barWidth - currentHealthWidth) / 2.0f, barY, currentHealthWidth, barHeight, 1.0f, 0.0f, 0.0f, 1.f);
-		DrawHollowRect(barX + xCam, barY, barWidth, barHeight, 1.f, 1.f, 0.f, 1.f);
+		m_Bosses[0]->DrawBossHPUI();
 	}
+	//if (m_Boss.IsAttackable())
+	//{
+	//	const float barWidth = 500.f;
+	//	const float barHeight = 125.f;
+	//	const float barX = 0;
+	//	const float barY = kHalfWindowHeight - 150.f;
+	//	DrawRect(barX + xCam, barY, barWidth, barHeight, 0.1f, 0.1f, 0.1f, 1.f);
+	//	float healthRatio = static_cast<float>(m_Boss.GetHealth()) / m_Boss.getMaxHealth();
+	//	float currentHealthWidth = barWidth * healthRatio;
+	//	DrawRect(barX + xCam - (barWidth - currentHealthWidth) / 2.0f, barY, currentHealthWidth, barHeight, 1.0f, 0.0f, 0.0f, 1.f);
+	//	DrawHollowRect(barX + xCam, barY, barWidth, barHeight, 1.f, 1.f, 0.f, 1.f);
+	//}
 }
 
 ACharacter* MainGameState::FindClosestEnemyInFront()
